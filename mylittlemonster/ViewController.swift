ad00e25e5bb7eb975ddaad6790f63e8b4ba83509
spentak <mprice@verisagellc.com>
2015-08-06 23:19:19 -0600

    Initial Commit

---
+++ mylittlemonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mylittlemonster
+//
+//  Created by Mark Price on 8/6/15.
+//  Copyright © 2015 devslopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
